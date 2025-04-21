bb163583ae436b6c79925c0c608946679912c8e7
Nikola Begedin <begedinnikola@gmail.com>
2025-04-21 06:32:17 +0200

    Final poncho unbreaking

--- lib/snake/wx_records.ex
+++
@@ -1,7 +0,0 @@
-defmodule Snake.GL.WxRecords do
-  require Record
-
-  for {type, record} <- Record.extract_all(from_lib: "wx/include/wx.hrl") do
-    Record.defrecord(type, record)
-  end
-end
--- priv/shaders/vertex.vs
+++
@@ -1,9 +0,0 @@
-#version 410 core
-
-layout(location = 0) in vec3 position;
-
-uniform mat4 projection;
-
-void main() {
-  gl_Position = projection * vec4(position, 1.0);
-}
